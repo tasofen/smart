8a55e02f4d7aaf5bc0182f0d92f64d9213f96a51
Tikhonov Aleksandr <123546.90@mail.ru>
2022-12-12 02:36:29 +0300

    save

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ssd
+/usr/sbin/smartctl -a /dev/sda \
+	| grep -v "Local Time is:" \
+	| grep -v Total_LBAs_Written \
+	| grep -v Power_On_Hours \
+	| grep -v Airflow_Temperature_Cel \
+	> sda.log
+
+# hdd 5400
+/usr/sbin/smartctl -a /dev/sdb \
+	| grep -v "Local Time is:" \
+	| grep -v Temperature_Celsius \
+	| grep -v Power_On_Hours \
+	> sdb.log
+
+git add .
+git commit -m "save"
+git push origin master
+

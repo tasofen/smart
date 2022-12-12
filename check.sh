7155dc92228d41e297e58edf01e097b3446116b3
Tikhonov Aleksandr <123546.90@mail.ru>
2022-12-12 04:00:02 +0300

    save

--- check.sh
+++ check.sh
@@ -2,18 +2,18 @@
 
 # ssd
 /usr/sbin/smartctl -a /dev/sda \
-	| grep -v "Local Time is:" \
-	| grep -v Total_LBAs_Written \
-	| grep -v Power_On_Hours \
-	| grep -v Airflow_Temperature_Cel \
-	> sda.log
+    | grep -v "Local Time is:" \
+    | grep -v Total_LBAs_Written \
+    | grep -v Power_On_Hours \
+    | grep -v Airflow_Temperature_Cel \
+    > sda.log
 
 # hdd 5400
 /usr/sbin/smartctl -a /dev/sdb \
-	| grep -v "Local Time is:" \
-	| grep -v Temperature_Celsius \
-	| grep -v Power_On_Hours \
-	> sdb.log
+    | grep -v "Local Time is:" \
+    | grep -v Temperature_Celsius \
+    | grep -v Power_On_Hours \
+    > sdb.log
 
 git add .
 git commit -m "save"
